2c1677a2b5e5ce35cceac677da66f9840d4c3717
LeQuangVinh-19503031 <lea26462@gmail.com>
2022-04-29 16:06:31 +0700

    Xoa

--- JavaScripts/Menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('button').click(function(){
-        $('ul').toggleClass('active');
-    })
-})
-window.addEventListener("click", function(){
-        if (!event.target.matches('#frames-menu')){
-            $('ul').removeClass('active')}
-    });
--- JavaScripts/Test.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("p2").style.color = "blue";
-document.getElementById("p2").style.fontFamily = "Arial";
-document.getElementById("p2").style.fontSize = "larger";
